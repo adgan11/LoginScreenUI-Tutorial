741864975f96b2d410c777d5e9c6fb2b66beac41
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2022-12-31 14:50:09 +0530

    Initial Commit

---
+++ LoginScreenTut/LoginScreenTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenTutApp.swift
+//  LoginScreenTut
+//
+//  Created by Ahmed Gagan on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
